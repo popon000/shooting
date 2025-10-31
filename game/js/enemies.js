8cd0d3449cdaf61e32ec28805a01e7756bb75db9
popon000 <hyp-880-zrw@e-net.nara.jp>
2025-10-31 06:21:13 +0000

    10_31

--- game/js/enemies.js
+++ game/js/enemies.js
@@ -4,8 +4,8 @@ const SIZE =26;
 function pushEnemy(canvas) {
     const w = SIZE;
     const h = SIZE;
-    const x = Math.random() * (canvas.Width - w);
-    const y = -h;
+    const x = Math.random() * (canvas.width - w);
+    const y = 0;
     const vy = 5;
     enemies.push({ x, y, width: w, height: h, vy });
 }
@@ -16,19 +16,19 @@ export function spawnEnemy(canvas) {
     }
 }
 
-//export function updateEnemies(canvas) {
- //  for (let i = enemies.length - 1; i>=0;i-- ) {
-    //    const e = enemies[i];
-      //  e.y += e.vy;
-        //if (e.y > canvas.height) {
-          //  enemies.splice(i, 1);
-        //}
-    //}
-//}
+export function updateEnemies(canvas) {
+  for (let i = enemies.length - 1; i>=0;i-- ) {
+       const e = enemies[i];
+       e.y += e.vy;
+        if (e.y > canvas.height) {
+           enemies.splice(i, 1);
+        }
+    }
+}
 
-//export function drawEnemies (ctx) {
-  //  ctx.fillStyle = "crimson";
-    //for (const e of enemies) {
-      //  ctx.fillRect (e.x, e.y, e.width, e.height);
-    //}
-//}
+export function drawEnemies (ctx) {
+   ctx.fillStyle = "crimson";
+    for (const e of enemies) {
+       ctx.fillRect (e.x, e.y, e.width, e.height);
+    }
+}
--- game/js/main.js
+++ game/js/main.js
@@ -1,74 +1,74 @@
 
-import { player , initPlayer , drawPlayer } from "./player.js";
-import { spawnEnemy, enemies,  } from "./enemies.js";
+
+import { player, initPlayer, drawPlayer } from "./player.js";
+import { spawnEnemy, enemies,updateEnemies,drawEnemies} from "./enemies.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 initPlayer(canvas);
-spawnEnemy(canvas);
 
-const bullets = [];
-const BULLET_SPEED = -10;
+
+export const bullets = [];
+const BULLET_SPEED = -5;
+
 function tryShoot() {
-  bullets.push({
-    x: player.x,
-    y: player.y,
-    width: 5,
-    height: 5,
-    vy: BULLET_SPEED,
-  })
+    bullets.push({
+        x: player.x,
+        y: player.y,
+        width: 10,
+        height: 10,
+        vy: BULLET_SPEED,
+    })
 }
-
-
 
 
 window.addEventListener("keydown", (e) => {
- if (e.key === "ArrowLeft") {
-   if(player.x > 10)
-  player.x -= 10;
- }else if (e.key === "ArrowRight") {
-   if(player.x < canvas.width - player.width - 10)
-  player.x += 10;
- }else if (e.code === "Space") {
-   tryShoot();
- }      
+    if (e.key === "ArrowLeft") {
+        if (player.x > 10) {
+            player.x -= 10;
+        }
+    } else if (e.key === "ArrowRight") {
+        if (player.x < canvas.width - player.width - 10) {
+            player.x += 10;
+        }
+    } else if (e.code === "Space") {
+        tryShoot();
+    }
 });
 
 function update() {
-  for (let i = 0; i < bullets.length; i++) {
-    const bullet = bullets[i];
-    bullet.y += bullet.vy;
-    if (bullet.y < 0) {
-      bullets.splice(i, 1);
+    for (let i = 0; i < bullets.length; i++) {
+        const bullet = bullets[i];
+        bullet.y += bullet.vy;
+        if (bullet.y < 0) {
+            bullets.splice(i, 1);
+        }
     }
-  }
+    spawnEnemy(canvas);
+    updateEnemies(canvas);
+    handleCollisions();
 }
 
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    drawPlayer(ctx);
 
-function draw() { 
-  ctx.fillStyle = "black";
- ctx.fillRect(0, 0, 480, 640);
-
-drawPlayer(ctx);
-
- for (let i = 0; i < bullets.length; i++) {
-  const bullet = bullets[i];
-   ctx.fillStyle = "white";
-   ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
- }
- for (let i = 0; i < enemies.length; i++) {
-  const enemy = enemy[i];
-   ctx.fillStyle = "rde";
-   ctx.fillRect(enemy.x, enemy.y, enemy.width,enemy.height);
-}
+    ctx.fillStyle = "white";
+    for (let i = 0; i < bullets.length; i++) {
+        const bullet = bullets[i];
+        ctx.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+    }
+    drawEnemies(ctx);
+    spawnEnemy(canvas);
 }
 
 function gameLoop() {
-  update();
-  draw();
-  requestAnimationFrame(gameLoop);
+    update();
+    draw();
+    requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
